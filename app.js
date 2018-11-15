0c9b3c1a56162f4030e1b0c3aebf0a616f905ed9
liyiwei <491027175@qq.com>
2018-11-15 18:22:15 +0800

    dasdas

--- app.js
+++ app.js
@@ -40,7 +40,14 @@ app.post('/post',function(req,res){
 	res.header('Access-Control-Allow-Origin', '*')
 	res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With')
 	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-	console.log(req.body);
+	let params = req.body
+	params.tags.filter(tags=>tags)
+	let vals = Object.values(params)
+	let isEmpty = !vals.every(val=> val)
+	if(isEmpty){
+		return res.json({"code":100,msg:"输入不能为空"})
+	}
+
 	const Flutter = Model.Flutter
 	new Flutter(req.body).save(function(){
 		res.json({msg:'success'})
