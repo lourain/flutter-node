c0dee1cd118a73c5b7c66b417a96601f11c76e48
lyw <491027175@qq.com>
2018-12-10 19:22:19 +0800

    photo

--- route/backend_route/albums.js
+++ route/backend_route/albums.js
@@ -2,14 +2,10 @@ const express = require('express')
 const router = express.Router()
 const {Pic} = require('../../mongo/model')
 
+
 router.get('/',function(req,res){
-    new Pic().get({},{album_name:1,_id:0},function(albums){
-        // let data = []
-        // albums.forEach(album => {
-        //     data.concat(Object.values(album))
-        // });
-        // console.log(albums,data);
-        
+    new Pic().get({},{album_name:1,urls:1,_id:0},function(albums){
+
         res.json({"code":0,data:albums})
     })
 })
