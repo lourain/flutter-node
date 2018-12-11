e5f464d7c8994e26ac13c73f6e48bd29869f7d73
lyw <491027175@qq.com>
2018-12-11 18:22:40 +0800

    电饭锅蛋糕

--- route/backend_route/albums.js
+++ route/backend_route/albums.js
@@ -4,10 +4,17 @@ const {Pic} = require('../../mongo/model')
 
 
 router.get('/',function(req,res){
-    new Pic().get({},{album_name:1,urls:1,_id:0},function(albums){
-
-        res.json({"code":0,data:albums})
-    })
+    let id = req.query.id
+    if(id){
+        new Pic().get({_id:id},null,function(albums){
+            res.json({"code":0,data:albums})
+        })
+    }else{
+        new Pic().get({},{album_name:1,urls:1,_id:1},function(albums){
+            res.json({"code":0,data:albums})
+        })
+    }
+    
 })
 
 module.exports = router
