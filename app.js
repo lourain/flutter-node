a09b69be2c9074cf2fa46359804da7d0dba4d6e3
liyiwei <491027175@qq.com>
2018-12-13 18:34:12 +0800

    sdfsdf

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const jwtAuth = expressJwt({
 		return req.headers.authorization
 	},
 	maxAge:60*60,//60min过期时间
-}).unless({ path: ['/login','/directory'] })
+}).unless({ path: ['/login','/detail','/directory'] })
 
 
 
