65bdc16bba57286fcc8d522c26b51d0d7c421bf6
lyw <491027175@qq.com>
2018-12-27 00:46:43 +0800

    jljlk

--- flutter/service-worker.js
+++ flutter/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aeadb99c34fa645c734cd550fddafde1.js"
+  "./precache-manifest.32a9eef980c649cf9fcb30db479a9b73.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
